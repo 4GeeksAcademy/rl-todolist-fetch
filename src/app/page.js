69b42804ab12dee2168d956b5cb42d5a99994df7
r63lopez <168450472+r63lopez@users.noreply.github.com>
2024-07-22 13:28:12 +0000

    Complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
+"use client"
+
 import Image from "next/image";
+import { TodoList } from "@/Components/TodoList";
 
 export default function Home() {
   return (
     
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">       
-      Hello RL APP
+      < TodoList user="user2" />
       </div>
   );
 }
---
+++ src/services/getUsers.js
@@ -0,0 +1,5 @@
+export async getSupportedArchTriples() {
+    const response = await fetch(
+    'https://playground.4geeks.com/todo/users/${user}'
+
+}
